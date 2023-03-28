7171b4ba34033035ca593cc59adaf961fe857417
Killian <paulic.killian@gmail.com>
2023-03-28 14:14:19 +0200

    getting started with the navbar position

---
+++ src/components/navigationBar/navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./navigation.css";
+
+export const NavigationBar = () => {
+  return (
+    <div className="navigation">
+      <div>logo</div>
+      <div>home</div>
+      <div>notifications</div>
+      <div>profile</div>
+      <div>image and logout</div>
+    </div>
+  );
+}
