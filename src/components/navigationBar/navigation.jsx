8970eaff9e4e600730efa0c5a250f03d79030a44
Killian <paulic.killian@gmail.com>
2023-03-29 11:19:45 +0200

    Color and layout updated

--- src/components/navigationBar/navigation.jsx
+++ src/components/navigationBar/navigation.jsx
@@ -4,29 +4,33 @@ import { IoLogoTwitter } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
 import { RiHome7Line } from "react-icons/ri";
 import { IoMdNotificationsOutline } from "react-icons/io";
-import { BsPersonFill } from "react-icons/bs";
+import { BsPerson } from "react-icons/bs";
 
 export const NavigationBar = () => {
   return (
     <div className="menu-container">
       <div className="navigation">
-          <div className="navigation-element">
-            <IoLogoTwitter color="var(--white)" />
+          <div className="navigation-element navigation-first-element">
+            <IoLogoTwitter color="var(--white)" size={24} />
           </div>
           <div className="navigation-element">
-            <RiHome7Line color="var(--white)" />
+            <RiHome7Line color="var(--white)" size={24} />
             Home
           </div>
           <div className="navigation-element">
-            <IoMdNotificationsOutline color="var(--white)" />
+            <IoMdNotificationsOutline color="var(--white)" size={24}/>
             Notifications
           </div>
           <div className="navigation-element">
-            <BsPersonFill color="var(--white)" />
+            <BsPerson color="var(--white)" size={24}/>
             Profile
           </div>
         <div className="navigation-bottom navigation-element">
             <CgProfile color="var(--white)"/>
+            <div>
+              <p className="navigation-name">Name</p>
+              <p className="navigation-username">@username</p>
+            </div>
         </div>
       </div>
     </div>
