3e39f40374343780b44821aea16d585d47769939
Killian <paulic.killian@gmail.com>
2023-03-30 11:37:16 +0200

    navigation basic style done

--- src/components/navigationBar/navigation.jsx
+++ src/components/navigationBar/navigation.jsx
@@ -11,23 +11,23 @@ export const NavigationBar = () => {
     <div className="menu-container">
       <div className="navigation">
           <div className="navigation-element navigation-first-element">
-            <IoLogoTwitter color="var(--white)" size={24} />
+            <IoLogoTwitter color="var(--white)" size={30} style={{ background: 'transparent' }}/>
           </div>
           <div className="navigation-element">
-            <RiHome7Line color="var(--white)" size={24} />
+            <RiHome7Line color="var(--white)" size={24} style={{ background: 'transparent' }}/>
             Home
           </div>
           <div className="navigation-element">
-            <IoMdNotificationsOutline color="var(--white)" size={24}/>
+            <IoMdNotificationsOutline color="var(--white)" size={24} style={{ background: 'transparent' }}/>
             Notifications
           </div>
           <div className="navigation-element">
-            <BsPerson color="var(--white)" size={24}/>
+            <BsPerson color="var(--white)" size={24} style={{ background: 'transparent' }}/>
             Profile
           </div>
         <div className="navigation-bottom navigation-element">
-            <CgProfile color="var(--white)"/>
-            <div>
+            <CgProfile color="var(--white)" style={{ background: 'transparent' }}/>
+            <div style={{ background: 'transparent'}}>
               <p className="navigation-name">Name</p>
               <p className="navigation-username">@username</p>
             </div>
