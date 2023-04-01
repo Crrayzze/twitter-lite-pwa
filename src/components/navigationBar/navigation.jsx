ff7596651c0e5a6eca3444a3844ef337d797c5c3
Killian <paulic.killian@gmail.com>
2023-04-01 15:43:38 +0200

    handle navigation

--- src/components/navigationBar/navigation.jsx
+++ src/components/navigationBar/navigation.jsx
@@ -5,27 +5,38 @@ import { CgProfile } from "react-icons/cg";
 import { RiHome7Line } from "react-icons/ri";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { BsPerson } from "react-icons/bs";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const NavigationBar = () => {
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleNavigationClick = (path) => {
+    if (location.pathname !== path) {
+      navigate(path);
+    }
+  }
+
   return (
     <div className="menu-container">
       <div className="navigation">
-          <div className="navigation-element navigation-first-element">
+          <div className="navigation-element navigation-first-element" onClick={() => handleNavigationClick("/")}>
             <IoLogoTwitter color="var(--white)" size={30} style={{ background: 'transparent' }}/>
           </div>
-          <div className="navigation-element">
+          <div className="navigation-element" onClick={() => handleNavigationClick("/")}>
             <RiHome7Line color="var(--white)" size={24} style={{ background: 'transparent' }}/>
             <div className="navigation-label">
               Home
             </div>
           </div>
-          <div className="navigation-element">
+          <div className="navigation-element" onClick={() => handleNavigationClick("/notifications")}>
             <IoMdNotificationsOutline color="var(--white)" size={24} style={{ background: 'transparent' }}/>
             <div className="navigation-label">
               Notifications
             </div>
           </div>
-          <div className="navigation-element">
+          <div className="navigation-element" onClick={() => handleNavigationClick("/profile")}>
             <BsPerson color="var(--white)" size={24} style={{ background: 'transparent' }}/>
             <div className="navigation-label">
               Profile
