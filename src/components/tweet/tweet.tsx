a7cc8dec52949f0ede83d0756a0310d332d66a55
Killian <paulic.killian@gmail.com>
2023-04-10 16:03:34 +0200

    tweet design

--- src/components/tweet/tweet.tsx
+++ src/components/tweet/tweet.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { TweetProps } from '../../interfaces/tweetProps';
+import { AiOutlineHeart } from 'react-icons/ai';
+import './tweet.css';
 
 export const Tweet: React.FC<TweetProps> = (tweet) => {
   return (
-    <div>
-      <div>
-        <p>PP</p>
-      </div>
-      <div>
-        <div>
-          <p>{tweet.author}</p>
-          <p>{tweet.date}</p>
+    <div className='tweet-container'>
+        <img className='tweet-profile-picture' src={ tweet.authorImage } alt="Profile avatar" />
+      <div className='tweet-align'>
+        <div className='tweet-top'>
+          <p className='tweet-author'>{tweet.author}</p>
+          <p className='tweet-info'>•</p>
+          <p className='tweet-info'>{tweet.date}</p>
         </div>
-        <p>{tweet.text}</p>
-        <div>
-          <p>{tweet.likes}</p>
-          <p>{tweet.localization}</p>
+        <p className='tweet-text'>{tweet.text}</p>
+        <div className='tweet-bottom'>
+          <div className='tweet-button-wrapper'>
+            <AiOutlineHeart className='tweet-info tweet-icon' />
+            <p className='tweet-info'>{tweet.likes}</p>
+          </div>
+          <p className='tweet-info'>•</p>
+          <p className='tweet-info'>{tweet.localization}</p>
         </div>
       </div>
     </div>
