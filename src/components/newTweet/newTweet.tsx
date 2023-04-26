3ad7734f132ac4b87ae9007c9dd452f961419955
Killian <paulic.killian@gmail.com>
2023-04-26 20:37:36 +0200

    starting the new component

--- src/components/newTweet/newTweet.tsx
+++ src/components/newTweet/newTweet.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './newTweet.css';
 
 export const NewTweet: React.FC = () => {
   return (
-    <div>
-      <h1>New Tweet</h1>
+    <div className='tweet-container'>
+      <img className='tweet-profile-picture' src="https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png" alt="Profile avatar" />
+      <div className='tweet-align'>
+        <textarea>What's happening?</textarea>
+      </div>
+      <button>Tweet</button>
     </div>
   );
 };
