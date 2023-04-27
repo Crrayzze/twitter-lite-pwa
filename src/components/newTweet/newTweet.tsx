1db6fddb38a13e6a6d072a390125a0e308bfbdbd
Killian <paulic.killian@gmail.com>
2023-04-27 19:57:39 +0200

    get geolocation

--- src/components/newTweet/newTweet.tsx
+++ src/components/newTweet/newTweet.tsx
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './newTweet.css';
 
 export const NewTweet: React.FC = () => {
 
   const [text, setText] = useState<string>('');
+  const [coordinates, setCoordinates] = useState({latitude: 0, longitude: 0});
+
+  const getGeolocation =  () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(pos => {
+        setCoordinates({
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude
+        })
+      })
+    }
+  }
 
   const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(event.target.value);
   }
 
-  const handleSubmit = () => {
-    console.log('Tweet: ', text);
-    // TODO: Use call to send the tweet
-    setText('')
+  const handleSubmit =  () => {
+    if (text.length > 0) {
+      console.log('Tweet: ', text);
+      console.log("coordinates: ", coordinates);
+      // TODO: Use call to send the tweet
+      setText('')
+    }
   }
+
+  useEffect(() => {
+    getGeolocation()
+  }, [coordinates]);
 
   return (
     <div className='tweet-container'>
