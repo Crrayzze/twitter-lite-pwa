074bfdf1f40fa63625626d65a3567314f38b9dc4
Enzo Surget <enzo.surget@epitech.eu>
2023-05-05 19:46:45 +0200

    feat: add logo and link to register/login in auth page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthFirebase } from '../../data/firebase/auth';
-
+import { Link } from "react-router-dom";
+import "./login.css";
+import { IoLogoTwitter } from 'react-icons/io';
 
 export const Login: React.FC = () => {
   const navigate = useNavigate();
@@ -28,11 +30,18 @@ export const Login: React.FC = () => {
   return (
     <div className="form-container">
       <div className="login-form">
-        <h1 className="form-title">Login</h1>
+        <div className="title-wrapper">
+          <IoLogoTwitter color="var(--white)" size={32} style={{ background: 'transparent', marginTop: '2px' }}/>
+          <h1 className="form-title">Login</h1>
+        </div>
         <form onSubmit={handleLoginFormSubmit}>
           <input className="form-input" name="email" type="email" placeholder="Email" required />
           <input className="form-input" name="password" type="password" placeholder="Password" required />
           <button className="form-button" type="submit">Login</button>
+          <p className="register-text">
+            <span className='register-int'>Don't have an account ? </span>
+            <Link to={'/register'}>Register</Link>
+          </p>
         </form>
       </div>
     </div>
